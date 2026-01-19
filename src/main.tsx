9e5bdf88a941f64d7d85721eb28c81cdda899303
Alisson Caldeira <alissoncaldeira.d@gmail.com.br>
2026-01-19 16:03:39 -0300

    Vite Default React Components Deleted, Firt Component Created

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+// Importando o Elemento no React
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>  {/*Apenas para uso de Desenvolvimento*/}
+
     <App />
+
   </StrictMode>,
-)
+);
