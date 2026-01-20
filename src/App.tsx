f5e6da10beb53c3362f48374a461fa752151565c
Alisson Caldeira <alissoncaldeira.d@gmail.com.br>
2026-01-20 15:49:49 -0300

    Creating the global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-// PascalCase
+import './styles/global.css'
+import './styles/theme.css'
 
+// PascalCase
 export function App() { // Exportar o Funcion é mais fácil
     console.log("Hello");
     // O que aparecerá na página
@@ -8,7 +10,7 @@ export function App() { // Exportar o Funcion é mais fácil
         // Só é possível retornar UM elemento pai no jsx/tsx
         <> {/*Isso é um React Fragment, basicamente uma tag vazia que é pais de todos os itens
         desse componente. OBS: ela não aparece como elemento no inspencionar do navegador */}
-            <h1>Hello World (do App)</h1>
+            <h1>Hello World (from App)</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing
                 elit. Laborum aspernatur iure deserunt beatae doloru
                 m! Aliquam dolore doloribus voluptates quibusdam libero, sapiente,
