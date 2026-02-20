61a3da4f7926741c26f28c15ee048ad11d32ae91
Alisson Caldeira <alissoncaldeira.d@gmail.com.br>
2026-02-20 16:35:44 -0300

    Ref hook was added to input component for control of his text value

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -2,13 +2,24 @@ import { PlayCircleIcon } from "lucide-react"
 import { Button } from "../Button"
 import { Cycles } from "../Cycles"
 import { DefaultInput } from "../DefaultInput"
+import { useRef } from "react";
 
 export function MainForm() {
 
+    const taskNameInput = useRef<HTMLInputElement>(null);
+
+    function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
     return (
-        <form className='form' action="">
+        <form onSubmit={handleCreateNewTask} className='form' action="">
             <div className="formRow">
-                <DefaultInput id='meuInput' labelText='Task' type='text' placeholder='Digite Algo' />
+                <DefaultInput
+                    id='meuInput'
+                    labelText='Task'
+                    type='text'
+                    placeholder='Digite Algo'
+                    ref={taskNameInput} />
             </div>
             <div className="formRow">
                 <span>Lorem ipsum dolor sit amet.</span>
